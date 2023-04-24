7c1c170ec4f2a7f462ed2723f5c53af99528a36c
Ola Wistedt <ola@witech.se>
2023-04-24 16:14:32 +0200

    Ifdeffed away code that skips gui updating.

--- BassMatrix/BassMatrix.cpp
+++ BassMatrix/BassMatrix.cpp
@@ -638,12 +638,12 @@ void BassMatrix::ProcessMidiMsg(const IMidiMsg& msg)
 #if IPLUG_DSP
 void BassMatrix::OnParamChangeUI(int paramIdx, EParamSource source)
 {
-#ifndef WAM_API
+#ifdef VST3_API
   if (source != kUI && source != kReset && source != kPresetRecall && source != kHost)
   {
     return;
   }
-#endif // WAM_API
+#endif // API
 
   double value = GetParam(paramIdx)->Value();
 
